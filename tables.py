f41317f4ac376474d76da5d5d412bfa51ad40da0
mustafaon01 <mustafaoncu815@gmail.com>
2023-03-24 06:35:32 +0300

    tables updated

---
+++ tables.py
@@ -0,0 +1,175 @@
+universities_table = '''
+                    CREATE TABLE IF NOT EXIST universities(
+                    PROGRAM_KODU INT,
+                    YIL INT,
+                    UNIVERSITE_ADI VARCHAR(500),
+                    FAKULTE VARCHAR(500),
+                    BOLUM_ADI VARCHAR(500)
+ )'''
+
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST general_information(
+                    PROGRAM_KODU INT,
+                    YIL INT,
+                    UNI_TYPE VARCHAR(100),
+                    PUAN_TURU VARCHAR(100),
+                    BURS_TURU VARCHAR(100),
+                    GENEL_KONTENJAN INT,
+                    OKUL_BIRINCISI_KONTENJANI INT,
+                    TOPLAM_KONTENJAN INT,
+                    GENEL_KONTENJANA_YERLESEN INT,
+                    OKUL_BIRINCISI_KONTENJANINA_YERLESEN INT,
+                    TOPLAM_YERLESEN INT,
+                    BOS_KALAN_KONTENJAN INT,
+                    ILK_YERLESME_ORANI VARCHAR(50),
+                    YERLESIP_KAYIT_YAPTIRMAYAN INT,
+                    EK_YERLESEN INT,
+                    
+                    YERLESEN_SON_KISI VARCHAR(100),
+                    )
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+gender_distribution_table = '''
+                    CREATE TABLE IF NOT EXIST gender_distribution(
+                    PROGRAM_KODU INT,
+                    YIL INT,
+                    CINSIYET VARCHAR(8),
+                    SAYI INT,
+                    ORAN VARCHAR(50) 
+                    )
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+general_information_table = '''
+                    CREATE TABLE IF NOT EXIST universities
+
+
+
+'''
+
